e03b226b7d05d6e804b5c801cdcf0ca9c1bc4e86
Radek Sedlák <r.sedlak@quadient.com>
2025-09-03 10:03:29 +0200

    PERF: optimized WTR support and config resolution

--- src/main/kotlin/com/github/sedlakr/webtestrunnerjetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sedlakr/webtestrunnerjetbrainsplugin/services/MyProjectService.kt
@@ -1,15 +1,93 @@
 package com.github.sedlakr.webtestrunnerjetbrainsplugin.services
 
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.VirtualFile
+import java.io.File
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
+    var workingDir: VirtualFile? = null
+    var wtrInfoInitialized = false;
+    var isWithWtrPluginRunner = false
+    var isSupported = false;
 
     init {
         thisLogger().info("AAAHOJ" + project.name)
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
+    fun wtrSupportedInfo(): Pair<Boolean, Boolean> {
+        if (!wtrInfoInitialized) {
+            throw Exception("WTR info not initialized yet")
+        }
+        return Pair(isSupported, isWithWtrPluginRunner);
+    }
+
+    fun wtrInfo(project: Project, testedVirtualFile: VirtualFile) {
+        if (wtrInfoInitialized) {
+            return
+        }
+        thisLogger().info("Detect support of WTR")
+        wtrInfoInitialized = true;
+        val wd = getWorkingDir(project, testedVirtualFile)
+        workingDir = wd;
+        // check if file wtr.config.mjs exists
+        val config = File(wd.path).resolve("wtr.config.mjs")
+
+        val exists = config.exists()
+        // not exist, not supported
+        if (!exists) {
+            isSupported = false;
+            isWithWtrPluginRunner = false;
+            thisLogger().info("Detect support of WTR - wtr.config.mjs not found")
+            return;
+        }
+        // supported, decide wtr.plugin.runner.js or fallback to peon
+        val wtrPluginRunner = File(wd.path).resolve("wtr.plugin.runner.js");
+
+        val isWithWtrPluginRunner = wtrPluginRunner.exists();
+        isSupported = true;
+        thisLogger().info("Detect support of WTR - supported, wtr.plugin.runner.js exists: " + isWithWtrPluginRunner)
+        this.isWithWtrPluginRunner = isWithWtrPluginRunner;
+    }
+
+    fun getProjectWorkingDir(): VirtualFile {
+        if (workingDir == null) {
+            throw Exception("Working dir not initialized yet")
+        }
+        return workingDir as VirtualFile;
+    }
+
+    companion object {
+        fun getInstance(project: Project): MyProjectService {
+            return project.getService<MyProjectService>(MyProjectService::class.java)
+        }
+
+        fun getWorkingDir(project: Project, testedVirtualFile: VirtualFile): VirtualFile {
+            var wd = project.guessProjectDir()!!
+
+            // Check if "project.js" exists in the guessed working directory
+            val projectJs = wd.findChild("project.js")
+            if (projectJs != null) {
+                return wd
+            }
+            var packageJson = wd.findChild("package.json")
+            if (packageJson != null) {
+                return wd
+            }
+
+            packageJson = PackageJsonUtil.findUpPackageJson(testedVirtualFile)
+            if (packageJson != null) {
+                val packageJsonDir = packageJson.parent
+                if (packageJsonDir != wd) {
+                    wd = packageJsonDir
+                }
+            }
+            return wd
+
+        }
+    }
 }
